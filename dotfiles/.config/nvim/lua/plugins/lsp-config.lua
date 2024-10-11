50ce69ec4f06a3b86aaa0fa0828829d6bcc7cdf0
James Pearson <j.k.pearson87@gmail.com>
2024-10-11 02:07:59 +0100

    Added debugging to nvim

--- dotfiles/.config/nvim/lua/plugins/lsp-config.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp-config.lua
@@ -7,23 +7,36 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "clangd",
+                    "arduino_language_server",
+                    "pylsp"
+                }
+            })
+        end
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local defaultCapabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
-                capabilities = defaultCapabilities
+                capabilities = capabilities
             })
             lspconfig.clangd.setup({
-                capabilities = defaultCapabilities
+                capabilities = capabilities
+            })
+            lspconfig.arduino_language_server.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pylsp.setup({
+                capabilities = capabilities
             })
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     },
