392045c0e3f6222e475e15aae882711d8600f461
James Pearson <j.k.pearson87@gmail.com>
2024-11-23 16:55:22 +0000

    added symlinks

--- dotfiles/.config/nvim/lua/plugins/catppuccin.lua
+++ dotfiles/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,1 @@
-return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-                transparent_background = true,
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end
-}
+/home/james/.config/nvim/lua/plugins/catppuccin.lua
--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,1 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
-}
-
+/home/james/.config/nvim/lua/plugins/treesitter.lua
