5a580240aeb58c9fe0f9171a895a14dfb6c06deb
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-03-02 20:13:25 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ echo ":: Dev Projects cloned"
 
 # Run package installation
 echo ":: Installing required packages..."
-bash ~/Dev/hyprland/scripts/install_packages.sh
+source scripts/install_packages.sh
 
 # Ensure .config directory exists
 mkdir -p ~/.config
