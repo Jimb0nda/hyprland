d028fdf363d78cb2619f2c5254ed9aff325d4dbb
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-03-02 20:24:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 
+# Run package installation
+echo ":: Installing yay..."
+source scripts/install_yay.sh
+
+# Run package installation
+echo ":: Installing required packages..."
+source scripts/install_packages.sh
