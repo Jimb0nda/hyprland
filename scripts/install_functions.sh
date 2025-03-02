940f536410a6761c47f82c6a453e7be5d45b81bc
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-03-02 20:40:46 +0000

    Update install_functions.sh

--- scripts/install_functions.sh
+++ scripts/install_functions.sh
@@ -1,1 +1,68 @@
+#!/bin/bash
+
+# Install Yay if not found
+_installYay() {
+    if ! command -v yay &>/dev/null; then
+        echo "yay not found! Installing..."
+        git clone https://aur.archlinux.org/yay-git.git ~/yay-git
+        cd ~/yay-git
+        makepkg -si --noconfirm
+        cd ~
+        rm -rf ~/yay-git
+        echo "yay installed successfully."
+    else
+        echo "yay is already installed."
+    fi
+}
+
+# Check if package is installed
+_isInstalled() {
+    package="$1";
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+# Install required packages
+_installPackages() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalled "${pkg}") == 0 ]]; then
+            echo "${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All pacman packages are already installed.";
+        return;
+    fi;
+    printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}";
+}
+
+_installPackagesYay() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledYay "${pkg}") == 0 ]]; then
+            echo ":: ${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All packages are already installed.";
+        return;
+    fi;
+
+    # printf "AUR packags not installed:\n%s\n" "${toInstall[@]}";
+    yay --noconfirm -S "${toInstall[@]}";
+}
+
+
 
