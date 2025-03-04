c89bf8a4be2d63bfaad7f1203389337605598481
James Pearson <j.k.pearson87@gmail.com>
2025-03-04 16:06:30 +0000

    symlink script

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ source scripts/install_functions.sh
 # Run package installation
 echo ":: Installing required packages..."
 source scripts/install_packages.sh
+
+
+#Run symlink script
+echo ":: running Symlink Script"
+source scripts/create_symlinks.sh
