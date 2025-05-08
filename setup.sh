b1f89e3ac056349fecb4397c5b6c95856b5f2613
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-05-08 17:22:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,24 +41,7 @@ echo ":: Dev Projects cloned"
 mkdir -p ~/.config
 
 cd hyprland
-#./install.sh
-
-# Get functions ready
-source scripts/install_functions.sh
-
-# Run package installation
-echo ":: Installing required packages..."
-source scripts/install_packages.sh
-
-
-#Run symlink script
-echo ":: running Symlink Script"
-sleep 1
-if [ -f scripts/create_symlinks.sh ]; then
-    source scripts/create_symlinks.sh
-else
-    echo "Error: create_symlinks.sh not found!"
-fi
+./install.sh
 
 # Reload Bash configuration
 source ~/.bashrc
