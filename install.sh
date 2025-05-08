9bae4ed35897bf2027850a104228b2d418b4655a
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-05-08 18:05:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ source scripts/install_packages.sh
 
 
 #Run symlink script
-echo ":: running Symlink Script"
-sleep 1
-if [ -f scripts/create_symlinks.sh ]; then
-    source scripts/create_symlinks.sh
-else
-    echo "Error: create_symlinks.sh not found!"
-fi
+#echo ":: running Symlink Script"
+#sleep 1
+#if [ -f scripts/create_symlinks.sh ]; then
+#    source scripts/create_symlinks.sh
+#else
+#    echo "Error: create_symlinks.sh not found!"
+#fi
