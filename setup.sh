8054d24de3cb1dce960ab774117fc7152c69f45b
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-05-08 17:45:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,16 +16,24 @@ cat <<"EOF"
 
 EOF
 
+echo -e "${NONE}"
+
 echo ":: Checking Git is installed"
+
+echo -e "${GREEN}"
+
 # Check if git is installed
 if ! command -v git &>/dev/null; then
+    echo ":: Installing Git"
     sudo pacman -S git --noconfirm
 fi
 
+echo -e "${NONE}"
 # Set up directories
 mkdir -p ~/Downloads ~/Dev
 echo ":: Directories ensured (Downloads, Dev)"
 
+echo -e "${GREEN}"
 # Clone repositories
 cd ~/Dev
 [[ -d "hyprland" ]] && rm -rf hyprland
@@ -36,12 +44,15 @@ git clone --depth 1 https://github.com/Jimb0nda/Cpp.git
 
 [[ -d "ML" ]] && rm -rf ML
 git clone --depth 1 https://github.com/Jimb0nda/ML.git
+echo -e "${NONE}"
 echo ":: Dev Projects cloned"
 
 # Ensure .config directory exists
 mkdir -p ~/.config
 
 echo ":: Entering Hyprland Folder for further install"
+echo -e "${GREEN}"
+
 cd hyprland
 ./install.sh
 
