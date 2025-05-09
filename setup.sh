0f8ea70f7d7496ad1dc4cb3beb544af782e3988f
James Pearson <48517231+Jimb0nda@users.noreply.github.com>
2025-05-09 18:21:22 +0100

    Changed info text

--- setup.sh
+++ setup.sh
@@ -237,11 +237,13 @@ git clone --depth 1 git@github.com:Jimb0nda/ML.git
 success ":: Dev Projects cloned and remote URLs set to SSH"
 
 # Setting up fingerprint service for enrollment after reboot
-info ":: Setting up fingerprint enrollment script for one time run after reboot"
+info "Setting up one-time fingerprint enrollment service..."
 chmod +x ~/Dev/hyprland/scripts/enroll_fingerprint.sh
 
 sudo cp ~/Dev/hyprland/config/enroll-fingerprint.service /etc/systemd/system/enroll-fingerprint.service
 sudo systemctl enable enroll-fingerprint.service
+success "Fingerprint enrollment service enabled for next boot."
+
 
 
 success ":: Setup complete!"
