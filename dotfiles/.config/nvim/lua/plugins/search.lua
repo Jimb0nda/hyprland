d56a51746007177ba925db4327a642e741e73ce5
James Pearson <j.k.pearson87@gmail.com>
2025-11-21 18:30:56 +0000

    Updated search for nvim

--- dotfiles/.config/nvim/lua/plugins/search.lua
+++ dotfiles/.config/nvim/lua/plugins/search.lua
@@ -5,16 +5,19 @@ return {
         config = function()
             local fzf = require("fzf-lua")
             fzf.setup({
-                files = { cwd_prompt = false },
+                defaults = {
+                    silent = true, -- hide startup/info messages
+                },
+                files = {
+                    cwd_prompt = false,
+                },
                 grep = {
                     rg_opts = "--hidden --glob '!.git/**' --glob '!node_modules/**' --glob '!bin/**' --glob '!obj/**'",
                 },
             })
-
             -- Keybinds similar to VS
-            vim.keymap.set("n", "<C-F>", fzf.files, { desc = "Find files" })
-            vim.keymap.set("n", "C-,", fzf.live_grep, { desc = "Live grep (project search)" })
+            vim.keymap.set("n", "<C-f>", fzf.files, { desc = "Find files" })
+            vim.keymap.set("n", "<C-,>", fzf.live_grep, { desc = "Live grep (project search)" })
         end,
     }
 }
-
